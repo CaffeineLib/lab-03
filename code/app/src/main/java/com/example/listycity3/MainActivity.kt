0da2cb81d312ebf8ee9ede3ba333fee2b505bbdd
Ben <cadets.ben@gmail.com>
2026-09-16 07:25:03 -0600

    finished step 2

--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -30,10 +30,17 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CityListScreen(
                         cities = cityRepository.cities,
+                        onAddCity = { cityRepository.addCity(it) },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+    }
+}
+
+fun String.toProperCase(): String {
+    return this.lowercase().split(" ").joinToString(" ") { word ->
+        word.replaceFirstChar { it.titlecase() }
     }
 }
